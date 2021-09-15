ff1a1fa68329672f725c5eaebc8ba132703f6875
daoxuandong <90700841+daoxuandong@users.noreply.github.com>
2021-09-15 14:49:37 +0700

    Add files via upload

---
+++ uocsochiahetcho2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+    cin>>t;
+    while (t--){
+	   	int n;cin>>n;
+	   	int i=1;
+	   	int dem =0;
+	   	while(i<=n/i){
+	   		if(n%i==0){
+	   			if(i%2==0) dem++;
+	   			if(n/i%2==0) dem++;
+	   			if(n/i==i&&i%2==0) dem--;
+			   }
+			   i++;
+		   }
+		   cout<<dem<<endl;
+	}   
+}
