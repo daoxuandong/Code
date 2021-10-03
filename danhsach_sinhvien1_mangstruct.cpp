65eb6b029949c0262032655c95b223198bb19b52
daoxuandong <90700841+daoxuandong@users.noreply.github.com>
2021-10-03 22:07:47 +0700

    Add files via upload

---
+++ danhsach_sinhvien1_mangstruct.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct SinhVien{
+    string name,lop,date;
+    float gpa;
+};
+void nhap(SinhVien ds[50],int n){
+    for(int i=0;i<n;i++){
+        scanf("\n");
+        getline(cin,ds[i].name);
+        cin>>ds[i].lop;
+        cin>>ds[i].date;
+        cin>>ds[i].gpa;
+        string res=ds[i].date;
+        if(res[1]<'0'||res[1]>'9') res='0'+res;
+        if( res[4] < '0' || res[4] > '9' ) res.insert(3,"0");
+        ds[i].date = res ;
+    }
+}
+void in(SinhVien ds[],int n){
+    for(int i=0;i<n;i++){
+        if(i<9){
+            cout<<"B20DCCN00"<<i+1<<" "<<ds[i].name<<" "<< ds[i].lop<<" "<<ds[i].date<<" ";
+            printf("%.2f",ds[i].gpa);
+            cout<<endl;
+        }
+        else{
+           cout<<"B20DCCN0"<<i+1<<" "<<ds[i].name<<" "<< ds[i].lop<<" "<<ds[i].date<<" ";
+            printf("%.2f",ds[i].gpa);
+            cout<<endl;
+        }
+    }
+}
+int main(){
+    struct SinhVien ds[50];
+    int N;
+    cin >> N;
+    nhap(ds, N);
+    in(ds, N);
+    return 0;
+}
